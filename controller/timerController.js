4920c0bdacb8a134003df26eb6dcca79666e20e9
Rémi <neptunes92390@outlook.fr>
2023-12-12 22:15:08 +0100

    foreign key for users and timers

--- controller/timerController.js
+++ controller/timerController.js
@@ -2,21 +2,27 @@ const User = require("../model/userModel");
 const Timer = require("../model/timerModel");
 
 exports.createATimes = async (req, res) => {
-    try {
-      await Timer.findById(req.params.user_id);
-      const newUser = new Timer({
+  try {
+    const user = await User.findById(req.params.user_id);
+
+    if (!user) {
+      return res.status(404).json({ message: "Utilisateur inexistant" });
+    }
+
+
+      const newTimer = new Timer({
         ...req.body,
-        timer_id: req.params.user_id,
+        user_id: user._id,
       });
   
       try {
-        const Times = await newUser.save();
+        const Times = await newTimer.save();
         res.status(201).json(Times);
       } catch (error) {
         res.status(500).json({ message: "Erreur serveur (db)." });
       }
     } catch (error) {
-      res.status(500).json({ message: "Erreur serveur (post inexistant)." });
+      res.status(500).json({ message: "Erreur serveur (utilisateur inexistant)." });
     }
   };
   
--- model/timerModel.js
+++ model/timerModel.js
@@ -2,9 +2,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 let timerSchema = new Schema({
-  time: {
-    //mettre en datetime plus tard
-    type: Number,
+  time: { 
+    type: Date,
     required: true,
   },
   created_at: {
@@ -12,7 +11,8 @@ let timerSchema = new Schema({
     default: Date.now,
   },
   user_id: {
-    type: String,
+    type :Schema.Types.ObjectId,
+    ref: "User",
   },
 });
 
